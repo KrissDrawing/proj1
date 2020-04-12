ff9c23ccc1f1517e34a020fe08e1c4482e0dfff4
krissdrawing <krx95@o2.pl>
2020-04-12 19:34:23 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
